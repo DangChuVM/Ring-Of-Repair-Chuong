39859fae927133011bcad8ed95160be10934cd41
DangChuVM <akirahung905@gmail.com>
2025-08-26 21:08:53 +0700

    Ll

--- src/main/java/com/chuong/ring/rp/item/RingOfRepairItem.java
+++
@@ -1,9 +0,0 @@
-package com.chuong.ring.rp.item;
-
-import net.minecraft.item.Item;
-
-public class RingOfRepairItem extends Item {
-    public RingOfRepairItem(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/com/chuong/ringofrepair/init/ItemList.java
+++
@@ -1,8 +0,0 @@
-package com.chuong.ringofrepair.init;
-
-import net.minecraft.item.Item;
-
-public class ItemList
-{
-	public static Item ring_of_repair;
-}
